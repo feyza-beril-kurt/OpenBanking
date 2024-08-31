f91b4a08aea76e7f337abed5e0d0628d3ae69746
Feyza Beril Kurt <126516799+feyza-beril-kurt@users.noreply.github.com>
2024-08-31 20:30:13 +0300

    Delete WEB/BackEnd/Files/BlogMvcApp directory

--- WEB/BackEnd/Files/BlogMvcApp/BlogMvcApp/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace BlogMvcApp.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
